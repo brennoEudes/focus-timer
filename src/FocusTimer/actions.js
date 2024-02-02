84e2de3ba917d69f60729ba4ae6fdfbe38551f4d
“Brenno <brennoeudes@gmail.com>
2024-02-02 12:48:44 -0300

    countdown structure

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,10 +1,13 @@
 import state from "./state.js";
+import * as timer from "./timer.js";
 
 export function toggleRunning() {
   // console.log("ToggleRunning function!"); // p/ testar se está funcionando
 
   // altera o estado de running retornando true ou false:
   state.isRunning = document.documentElement.classList.toggle("running");
+
+  timer.countdown();
 }
 
 export function reset() {
--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,12 +1,23 @@
 import state from "./state.js";
 import * as el from "./elements.js";
 
+export function countdown() {
+  if (!state.isRunning) {
+    return
+  }
+
+  console.log("Iniciou!")
+
+  // setTimeout: método que executa uma fcn depois de um tempo (ms):
+  setTimeout(() => countdown(), 1000) // RECURSÃO: quando uma fcn chama ela msm! Precisa ser parada em algum mommento para não dar problema no código!
+}
+
 // Atualizando valores do contador:
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes; // nullish coalescing operator: verifica se o elemento é null!
   seconds = seconds ?? state.seconds;
 
   // atualiza min e sec c/ os elementos passados acima:
-  el.minutes.textContent = String(minutes).padStart(2 , "0"); // transforma em string e usa uma método string p/ definir o nº caracteres e o caracter inicial!
+  el.minutes.textContent = String(minutes).padStart(2, "0"); // transforma em string e usa uma método string p/ definir o nº caracteres e o caracter inicial!
   el.seconds.textContent = String(seconds).padStart(2, "0");
 }
