134fe03cc4ae91d27c60d3e22391c4874d7bd760
“Brenno <brennoeudes@gmail.com>
2024-02-19 23:40:11 -0300

    github audio paths

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -1,8 +1,8 @@
 // pegando os sons e deixando disponíveis p/usarmos na aplicação:
-export const buttonPressAudio = new Audio("/assets/button-press.wav");
+export const buttonPressAudio = new Audio("https://brennoeudes.github.io/focus-timer/assets/button-press.wav");
 
 export const kitchenTimer = new Audio("https://brennoeudes.github.io/focus-timer/assets/kichen-timer.mp3");
 
-export const bgAudio = new Audio("/assets/bg-audio.mp3");
+export const bgAudio = new Audio("https://brennoeudes.github.io/focus-timer/assets/bg-audio.mp3");
 
 bgAudio.loop = true; // áudio em loop!
