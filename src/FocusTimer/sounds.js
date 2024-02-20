6a4195d2f02005a2a4e7435a1a94dc8ed19522ba
“Brenno <brennoeudes@gmail.com>
2024-02-20 12:20:20 -0300

    audio links path updated

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -1,8 +1,8 @@
 // pegando os sons e deixando disponíveis p/usarmos na aplicação:
-export const buttonPressAudio = new Audio("https://brennoeudes.github.io/focus-timer/assets/button-press.wav");
+export const buttonPressAudio = new Audio("https://brennoeudes.github.io/pomodoro-timer/assets/button-press.wav");
 
-export const kitchenTimer = new Audio("https://brennoeudes.github.io/focus-timer/assets/kichen-timer.mp3");
+export const kitchenTimer = new Audio("https://brennoeudes.github.io/pomodoro-timer/assets/kichen-timer.mp3");
 
-export const bgAudio = new Audio("https://brennoeudes.github.io/focus-timer/assets/bg-audio.mp3");
+export const bgAudio = new Audio("https://brennoeudes.github.io/pomodoro-timer/assets/bg-audio.mp3");
 
 bgAudio.loop = true; // áudio em loop!
